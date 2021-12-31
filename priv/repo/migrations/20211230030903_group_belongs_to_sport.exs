1add7a94bb8ac6a6f8d52e223f03e6d42251235a
Sara Thornsberry <thornsberrysara@gmail.com>
2021-12-31 17:59:49 -0500

    Setting sport to enum

--- priv/repo/migrations/20211230030903_group_belongs_to_sport.exs
+++
@@ -1,9 +0,0 @@
-defmodule Rec.Repo.Migrations.GroupBelongsToSport do
-  use Ecto.Migration
-
-  def change do
-    alter table(:groups) do
-      add :sport_id, references(:sports)
-    end
-  end
-end
